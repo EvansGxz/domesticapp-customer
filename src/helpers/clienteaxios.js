4d4d82613cfc17f85df4580375bcb3fb982f892d
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-05-23 20:04:43 -0500

    login

--- src/helpers/clienteaxios.js
+++ src/helpers/clienteaxios.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const BASE_URI = "https://shielded-citadel-90167.herokuapp.com/";
 export const tokenKey = "Domesticapp";
 
-import axios from "axios";
 
 const clienteAxios = axios.create({
   baseURL: BASE_URI,
