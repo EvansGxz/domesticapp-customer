23912142acbeb37b19162a5232b0f42015a01ee0
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-05-23 22:03:35 -0500

    add login with credenials

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,28 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { BasicContainer } from "../styles/containers";
 import { ButtonStandard } from "../styles/buttons";
 import Logo from "../assets/IconLogin.svg";
-import {
-  Input,
+import Input, {
   Label,
   ContainerInput,
   Form,
   Title,
 } from "../styles/views/Login";
+import { useAuth } from "../context/auth-context";
 
-const Login = () => {
+
+export default function Login(){
+  const { login } = useAuth();
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    login(form);
+  }
+
+  function handleFormChange(event) {
+    const { name, value } = event.target;
+
+    setForm({ ...form, [name]: value });
+  }
   return (
     <BasicContainer>
       <img src={Logo} alt="logo-domesticapp" />
       <Title>Iniciar Sesión</Title>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <ContainerInput>
-          <Label>Email</Label>
-          <Input type="text" placeholder="Ingresa Email" />
+          <Input
+            id="email"
+            label="Email"
+            type="email"
+            placeholder="example@mail.com"
+            value={form.email}
+            onChange={handleFormChange}
+          />
         </ContainerInput>
         <ContainerInput>
-          <Label>Password</Label>
-          <Input type="text" placeholder="Ingresa Password" />
+          <Input
+            id="password"
+            label="Password"
+            type="password"
+            placeholder="******"
+            value={form.password}
+            onChange={handleFormChange}
+          />
         </ContainerInput>
         <ButtonStandard type="submit" color="azul">
           Iniciar Sesión
@@ -32,4 +61,3 @@ const Login = () => {
   );
 };
 
-export default Login;
