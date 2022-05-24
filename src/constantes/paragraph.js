6ef406ec03882ec881a8f18c07a9ddf49dc40ad1
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-05-24 16:29:59 -0500

    add terms

--- src/constantes/paragraph.js
+++ src/constantes/paragraph.js
@@ -6,4 +6,189 @@ export  const faqText =
 "Aquí encontraras una serie de preguntas frecuentes, esperamos te sean"+
 "de ayuda, o comunícate con el Servicio de Soporte y Ayuda";
 
+export const termsText1="El presente documento tiene por objeto establecer las Condiciones Generales"+
+"de Uso de las aplicaciones móviles y el servicio web a titularidad de"+
+"DOMESTICAPP S.A.S. (en adelante DOMESTICAPP) con domicilio social en"+
+"Medellín Colombia, dirección Calle 9 sur #79C-151 e inscrito en la Cámara de"+
+"Comercio de Medellín."
+
+export const termsText2=
+"DOMESTICAPP se reserva el derecho a modificar las presentes Condiciones de"+
+"Uso con el objeto de adecuarlas a la legislación vigente aplicable en cada"+
+"momento. Las presentes Condiciones de Uso no excluyen la posibilidad de que"+
+"determinados Servicios de las aplicaciones, por sus características"+
+"particulares, sean sometidos, además de a las Condiciones Generales de Uso,"+
+"a sus propias condiciones particulares de uso (en adelante las Condiciones"+
+"Particulares)."
+
+export const termsText3=
+"La Empresa podrá, en cualquier momento y sin necesidad de previo aviso,"+
+"realizar cambios y actualizaciones de las presentes Condiciones de Uso y de la"+
+"Política de Privacidad. Estos cambios serán publicados en la Web y en la/s"+
+"Aplicación/es y serán efectivos desde el momento de su publicación."
+
+export const termsText31=
+"Como consecuencia de lo anterior, el Usuario deberá revisar periódicamente si"+
+"hay cambios en estas Condiciones y, tanto si existe consentimiento expreso"+
+"como si no, si el Usuario continua usando el Servicio tras la publicación, ello"+
+"implica la aceptación y asunción de los mismos. En caso de que no esté de"+
+"acuerdo con las actualizaciones de las Condiciones de uso o de la Política de"+
+"Privacidad, podrá renunciar dejando de usar el Servicio."
+
+export const termsText4=
+"El acceso y descarga de la aplicación es gratuito salvo en lo relativo al coste"+
+"de la conexión a través de la red de telecomunicaciones suministrada por el"+
+"proveedor de acceso contratado por los usuarios."
+
+export const termsText5=
+"Determinados servicios son exclusivos para nuestros clientes y su acceso se"+
+"encuentra restringido."
+
+export const termsText6=
+"La descarga y uso de la aplicación atribuye la condición de usuario de la"+
+"misma (en adelante, el ‘Usuario’) e implica la lectura, entendimiento y"+
+"aceptación de todos los términos y condiciones"+
+"recogidas por la presente."
+
+export const termsText7=
+"El Usuario reconoce y acepta que el uso de los contenidos y/o servicios"+
+"ofrecidos por la presente aplicación móvil y/o portal web será bajo su"+
+"exclusivo riesgo y/o responsabilidad. El Usuario se compromete a utilizar la"+
+"presente aplicación móvil y portal web y todo su contenido y Servicios de"+
+"conformidad con la ley, la moral, el orden público y las presentes Condiciones"+
+"de Uso, y las Condiciones Particulares que, en su caso, le sean de aplicación."+
+"Así mismo, se compromete a hacer un uso adecuado de los servicios y/o"+
+"contenidos de la aplicación móvil y a no emplearlos para realizar actividades"+
+"ilícitas o constitutivas de delito, que atenten contra los derechos de terceros"+
+"y/o que infrinjan la regulación sobre propiedad intelectual e industrial, o"+
+"cualesquiera otras normas del ordenamiento jurídico aplicable."
+
+export const termsText8=
+"En particular, el Usuario se compromete a no trasmitir, introducir, difundir y"+
+"poner a disposición de terceros, cualquier tipo de material e información"+
+"(datos contenidos, mensajes, dibujos, archivos de sonido e imagen, fotografías,"+
+"software, etc.) que sean contrarios a la ley, la moral, el orden público y las"+
+"presentes Condiciones de Uso y, en su caso, a las Condiciones Particulares"+
+"que le sean de aplicación. A título enunciativo, y en ningún caso limitativo o"+
+"excluyente, el Usuario se compromete a:"
+
+export const termsText8_1=
+"– No introducir o difundir contenidos o propaganda de carácter racista,"+
+"xenófobo, pornográfico, de apología del terrorismo o que atenten contra los"+
+"derechos humanos."
+
+export const termsText8_2=
+"– No introducir o difundir en la red programas de datos (virus y software"+
+"nocivo) susceptibles de provocar daños en los sistemas informáticos del"+
+"proveedor de acceso, sus proveedores o terceros usuarios de la red Internet."
+
+export const termsText8_3=
+"– No difundir, transmitir o poner a disposición de terceros cualquier tipo de"+
+"información, elemento o contenido que atente contra los derechos"+
+"fundamentales y las libertades públicas reconocidos constitucionalmente y en"+
+"los tratados internacionales."
+
+export const termsText8_4=
+"– No difundir, transmitir o poner a disposición de terceros cualquier tipo de"+
+"información, elemento o contenido que constituya publicidad ilícita o desleal."
+
+export const termsText8_5=
+"– No transmitir publicidad no solicitada o autorizada, material publicitario,"+
+"“correo basura”, “cartas en cadena”, “estructuras piramidales”, o cualquier otra"+
+"forma de solicitación, excepto en aquellas áreas (tales como espacios"+
+"comerciales) que hayan sido exclusivamente concebidas para ello."
+
+export const termsText8_6=
+"– No introducir o difundir cualquier información y contenidos falsos, ambiguos o"+
+"inexactos de forma que induzca a error a los receptores de la información."
+
+export const termsText8_7=
+"– No suplantar a otros usuarios utilizando sus claves de registro a los distintos"+
+"servicios y/o contenidos de los Portales."
+
+export const termsText8_8=
+"– No difundir, transmitir o poner a disposición de terceros cualquier tipo de"+
+"información, elemento o contenido que suponga una violación de los derechos"+
+"de propiedad intelectual e industrial, patentes, marcas o copyright que"+
+"correspondan a los titulares de los Portales o a terceros."
+
+export const termsText8_9=
+"– No difundir, transmitir o poner a disposición de terceros cualquier tipo de"+
+"información, elemento o contenido que suponga una violación del secreto de"+
+"las comunicaciones y la legislación de datos de carácter personal."
+
+//PROPIEDAD INTELECTUAL E INDUSTRIAL
+export const termsText10=
+"DOMESTICAPP se reserva todos los derechos sobre las marcas, nombres"+
+"comerciales o demás signos distintivos, las patentes, y la propiedad intelectual,"+
+"con respecto al contenido y diseño de la aplicación móvil y el portal web."
+
+export const termsText11=
+"Todos los contenidos de la misma, incluyendo sin carácter limitativo, textos,"+
+"gráficos, imágenes, su diseño y los derechos de propiedad intelectual que"+
+"pudieran corresponder a dichos contenidos, así como las marcas, nombres"+
+"comerciales o cualquier otro signo distintivo son propiedad de DOMESTICAPP,"+
+"quedando reservados todos los derechos sobre los mismos. Las marcas,"+
+"nombres comerciales o signos distintivos son titularidad de DOMESTICAPP, sin"+
+"que pueda entenderse que la descarga, acceso y uso de la aplicación o portal"+
+"web le atribuya ningún derecho sobre las citadas marcas, nombres"+
+"comerciales y/o signos distintivos."
+
+//EXCLUSION DE GARANTÍAS. RESPONSABILIDAD
+export const termsText12=
+"DOMESTICAPP no garantiza en todo momento la disponibilidad de acceso y"+
+"continuidad del funcionamiento de la presente aplicación móvil, portal web y"+
+"de sus servicios, por lo que DOMESTICAPP no será responsable, con los límites"+
+"establecidos en el Ordenamiento Jurídico vigente, de los daños y perjuicios"+
+"causados al Usuario como consecuencia de la indisponibilidad, fallos de"+
+"acceso y falta de continuidad de la presente aplicación móvil y portal web y"+
+"sus Servicios."
+
+export const termsText13=
+"DOMESTICAPP responderá única y exclusivamente de los Servicios que preste"+
+"por sí misma y de los contenidos directamente originados por la empresa e"+
+"identificados con su copyright."
+
+export const termsText14=
+"Dicha responsabilidad quedará excluida en los casos en que concurran causas"+
+"de fuerza mayor o en los supuestos en que la configuración de los dispositivos"+
+"del Usuario no sea la adecuada para permitir el correcto uso de los servicios"+
+"de Internet prestados por DOMESTICAPP."
+
+export const termsText15=
+"La descarga, acceso y uso de la aplicación en los dispositivos móviles o"+
+"similares y el acceso a su portal web no implica la obligación por parte de"+
+"DOMESTICAPP de controlar la ausencia de virus, gusanos o cualquier otro"+
+"elemento informático dañino. Corresponde al Usuario, en todo caso, la"+
+"disponibilidad de herramientas adecuadas para la detección y desinfección"+
+"de programas informáticos dañinos."
+
+//CONDUCTA DE LOS USUARIOS
+export const termsText16=
+"DOMESTICAPP no garantiza que los Usuarios de la presente aplicación móvil y"+
+"el portal web utilicen los contenidos y/o servicios del mismo de conformidad"+
+"con la ley, la moral, el orden público, ni las presentes Condiciones Generales y,"+
+"en su caso, las condiciones Particulares que resulten de aplicación. Asimismo,"+
+"no garantiza la veracidad y exactitud, exhaustividad y/o autenticidad de los"+
+"datos proporcionados por los Usuarios."
+
+export const termsText17=
+"DOMESTICAPP no será responsable, indirecta ni subsidiariamente, de los"+
+"daños y perjuicios de cualquier naturaleza derivados de la utilización de los"+
+"Servicios y Contenidos de la aplicación y portal web por parte de los Usuarios"+
+"o que puedan derivarse de la falta de veracidad, exactitud y/o autenticidad"+
+"de los datos o informaciones proporcionadas por los Usuarios, o de la"+
+"suplantación de la identidad de un tercero efectuada por un Usuario en"+
+"cualquier clase de actuación a través de la presente aplicación móvil o portal"+
+"web. Por lo tanto, el uso de esta aplicación y portal no implica la obligación"+
+"por parte de DOMESTICAPP de comprobar la veracidad, exactitud,"+
+"adecuación, idoneidad, exhaustividad y actualidad de la información"+
+"suministrada a través de la misma."
+
+export const termsText18=
+"DOMESTICAPP no se responsabiliza de las decisiones tomadas a partir de la"+
+"información suministrada a través de la aplicación y portal ni de los daños y"+
+"perjuicios producidos en el Usuario o terceros con motivo de actuaciones que"+
+"tengan como único fundamento la información obtenida en la aplicación."
+
 export default supportText;
---
+++ src/pages/profile/Terminos.jsx
@@ -0,0 +1,103 @@
+import { termsText1, termsText10, termsText11, termsText12, termsText13, termsText14, termsText15, termsText16, termsText17, termsText18, termsText2, termsText3, termsText31, termsText4, termsText5, termsText6, termsText7, termsText8, termsText8_1, termsText8_2, termsText8_3, termsText8_4, termsText8_5, termsText8_6, termsText8_7, termsText8_8, termsText8_9, termsText9 } from "../../constantes/paragraph";
+
+
+function Terminos(){
+  return(
+    <>
+      <p>1 - CONDICIONES GENERALES DE USO</p>
+  
+      <div>
+        <p>{termsText1}</p>
+      </div>
+      <div>
+        <p>{termsText2}</p>
+      </div>
+      <div>
+        <p>{termsText3}</p>
+      </div>
+      <div>
+        <p>{termsText31}</p>
+      </div>
+      <div>
+        <p>{termsText4}</p>
+      </div>
+      <div>
+        <p>{termsText5}</p>
+      </div>
+      <div>
+        <p>{termsText6}</p>
+      </div>
+
+
+      <p>2 - USO DE LA APLICACIÓN MÓVIL Y PORTAL WEB Y SUS SERVICIOS</p>
+      <div>
+        <p>{termsText7}</p>
+      </div>
+      <div>
+        <p>{termsText8}</p>
+         <div>
+          <p>{termsText8_1}</p>
+        </div>
+        <div>
+          <p>{termsText8_2}</p>
+        </div>
+        <div>
+          <p>{termsText8_3}</p>
+        </div>
+        <div>
+          <p>{termsText8_4}</p>
+        </div>
+        <div>
+          <p>{termsText8_5}</p>
+        </div>
+        <div>
+          <p>{termsText8_6}</p>
+        </div>
+        <div>
+          <p>{termsText8_7}</p>
+        </div>
+        <div>
+          <p>{termsText8_8}</p>
+        </div>
+        <div>
+          <p>{termsText8_9}</p>
+        </div>
+      </div>
+
+      <p>3 - PROPIEDAD INTELECTUAL E INDUSTRIAL</p>
+      <div>
+        <p>{termsText10}</p>
+      </div>
+      <div>
+        <p>{termsText11}</p>
+      </div>
+
+      <p>4 - EXCLUSION DE GARANTÍAS. RESPONSABILIDAD</p>
+      <div>
+        <p>{termsText12}</p>
+      </div>
+      <div>
+        <p>{termsText13}</p>
+      </div>
+      <div>
+        <p>{termsText14}</p>
+      </div>
+      <div>
+        <p>{termsText15}</p>
+      </div>
+
+      <p>5 - CONDUCTA DE LOS USUARIOS</p>
+      <div>
+        <p>{termsText16}</p>
+      </div>
+      <div>
+        <p>{termsText17}</p>
+      </div>
+      <div>
+        <p>{termsText18}</p>
+      </div>
+    </>
+    )
+  }
+  
+  export default Terminos;
