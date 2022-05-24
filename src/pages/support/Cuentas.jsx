9de9d44619aec506473638dcb85bd120bf8ff6fb
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-05-24 10:24:49 -0500

    add account page

---
+++ src/pages/support/Cuentas.jsx
@@ -0,0 +1,27 @@
+
+
+function Cuentas(){
+  return(
+    <>
+      <p>1.2 FAQS RELACIONADO CON MI CUENTA</p>
+  
+      <div>
+        <p>1.2.1 ¿Cómo se maneja mi información personal?</p>
+      </div>
+      <div>
+        <p>En Domesticapp tu información es muy importante para nosotros,
+           nunca divulgaremos tus datos con terceros de acuerdo con las 
+           políticas internacionales de protección de la información del usuario</p>
+      </div>
+      <div>
+        <p>1.2.2 ¿Cuáles son los beneficios de contar con una cuenta en Domesticapp?</p>
+      </div>
+      <div>
+        <p>Cuando te registras en Domesticapp accedes a descuentos, sorteos con tus servicios,
+           además, puedes compartir estos regalos al referir a tus amigos y familiares.</p>
+      </div>
+    </>
+    )
+  }
+  
+  export default Cuentas;
