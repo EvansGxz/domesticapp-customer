9a0ac2d29856ad9a7791599253dbe98b49fc9b7f
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-05-23 23:01:03 -0500

    add options

--- src/pages/Support.jsx
+++ src/pages/Support.jsx
@@ -5,6 +5,11 @@ return(
   <>
     <p>{supportText}</p>
     <h2>Temas de Ayuda</h2>
+    <p>Chat de Soporte</p>
+    <p>Necesitas ayuda con tu último servicio?</p>
+    <p>Tienes alguna duda?</p>
+    <p>Notificaciones</p>
+    <p>Daño en el Servicio o Accidentes Laborales</p>
   </>
 
   )
