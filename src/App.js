878cf0970c9702b16ba42bc7f3dce40864d0abe6
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-05-23 21:39:58 -0500

    add auth params

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import { Route, Routes } from "react-router-dom";
-import { GlobalStyle } from "./styles/GlobalStyle";
-import Home from "./pages/Home";
-import Welcome from "./pages/Welcome";
-import Login from "./pages/Login";
+
+import Authenticated from "./Auth/Authenticated";
+import { useAuth } from "./context/auth-context";
+import UnAuthenticated from "./Auth/UnAuthenticated";
 
 function App() {
-  return (
-    <>
-      <GlobalStyle />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/welcome" element={<Welcome />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </>
-  );
+  const { user } = useAuth();
+  console.log(user)
+  return user ? <Authenticated /> : <UnAuthenticated />;
 }
 
 export default App;
