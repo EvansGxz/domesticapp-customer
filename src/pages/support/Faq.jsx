b5d7783544710e3fefa4ad06b4e4ae55e4ec2335
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-05-24 10:24:08 -0500

    add faq page

---
+++ src/pages/support/Faq.jsx
@@ -0,0 +1,18 @@
+import faqText from "../../constantes/paragraph";
+
+function Support(){
+return(
+  <>
+    <p>{faqText}</p>
+    <h2>Tengo dudas con:</h2>
+    <p>Mis servicios</p>
+    <p>Mi cuenta</p>
+    <p>Empleados</p>
+    <p>Pagos</p>
+    <p>COVID-19</p>
+    <p>Dotación</p>
+  </>
+  )
+}
+
+export default Support;
