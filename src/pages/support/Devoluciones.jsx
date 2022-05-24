cfa913a19c0903471b8c95b126e21d1e41373cab
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-05-24 10:24:30 -0500

    add refound page

---
+++ src/pages/support/Devoluciones.jsx
@@ -0,0 +1,29 @@
+
+
+function Devoluciones(){
+  return(
+    <>
+      <p>1.6 FAQS RELACIONADO CON MIS DEVOLUCIONES</p>
+  
+      <div>
+        <p>1.6.1 ¿En que casos puedo acceder a una devolución?</p>
+      </div>
+      <div>
+        <p>Cuando por eventualidades en nuestros sistemas,
+          incumplimiento de labores u otras variables en el
+          servicio, no se cumple a cabalidad con lo establecido
+          dentro de nuestras políticas de calidad.</p>
+      </div>
+      <div>
+        <p>1.6.2 ¿Cómo realizo una petición de devolución?</p>
+      </div>
+      <div>
+        <p>En todas nuestras plataformas cuentas con
+          servicio de atención al cliente 24/7, ChatBot de
+          respuestas automatizadas y atención vía email.</p>
+      </div>
+    </>
+    )
+  }
+  
+  export default Devoluciones;
