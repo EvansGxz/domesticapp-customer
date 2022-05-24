e5876aafb612021718488e1212e5d1036e1108ab
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-05-23 19:55:02 -0500

    Add home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { useEffect } from "react";
+import { useAuth } from "../context/auth-context";
 
-const Home = () => {
-  return <div>home</div>;
-};
 
-export default Home;
+export default function Home() {
+  /*
+  document.body.style.transition = "background-color 1s ease-out";
+  document.body.style.backgroundColor = "white";
+  var color = "#0BBBEF";
+  */
+  const { categories, getCategories } = useAuth();
+
+  useEffect(() => {
+    getCategories();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <div>
+      {categories ? (
+        <>
+          {categories.map((category) => {
+            return (
+              <p>{category.category_name} </p>  
+            );
+          })}
+        </>
+      ) : (
+        <div>
+          <h2>Categories not found</h2>
+        </div>
+      )}
+    </div>
+  );
+}
+
