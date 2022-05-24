b8a764baf7eed57a70021070fe334877628af1b9
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-05-24 10:24:20 -0500

    add faq employee page

---
+++ src/pages/support/Empleados.jsx
@@ -0,0 +1,47 @@
+
+
+function Empleados(){
+  return(
+    <>
+      <p>1.3 FAQS RELACIONADO CON ASISTENTES Y EMPLEADOS</p>
+  
+      <div>
+        <p>1.3.1 ¿Con que experiencia cuentan los empleados y asistentes?</p>
+      </div>
+      <div>
+        <p>TODOS nuestros asistentes y empleados han pasado por un filtro exhaustivo
+           bilateral, realizamos un seguimiento del perfil laboral mediante nuestra 
+           inteligencia artificial "Mile" y posteriormente una evaluación psicológica 
+           donde verificamos uno a uno todos los datos suministrados, antecedentes y por 
+           supuesto su experticia y experiencia en el oficio. Recuerda que puedes estar al 
+           tanto de esta información en tiempo real en el perfil del empleado o asistente de 
+           tu interés.</p>
+      </div>
+      <div>
+        <p>1.3.2 ¿Qué leyes respaldan a los empleados y asistentes de Domesticapp?</p>
+      </div>
+      <div>
+        <p>Acogidos con un compromiso social global,
+            Domesticapp garantiza todas las prestaciones de ley,
+            el salario justo y los beneficios laborales vigentes en
+            cada país donde realiza operaciones comerciales a
+            cada uno de los miembros de su personal.</p>
+      </div>
+      <div>
+        <p>1.3.3 ¿Cómo influye la calificación realizada a los empleados y asistentes 
+        que atiendan mis servicios?</p>
+      </div>
+      <div>
+        <p>Con el animo de realizar una constante mejora de
+            nuestros servicios, incentivamos a nuestros empleados
+            y asistentes a siempre satisfacer las necesidades de
+            nuestros clientes y a que estas se vean reflejadas
+            según la calificación recibida. Nuestros mejores
+            empleados reciben múltiples regalos, beneficios y
+            formaciones especiales.</p>
+      </div>
+    </>
+    )
+  }
+  
+  export default Empleados;
