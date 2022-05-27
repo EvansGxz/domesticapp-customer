8787c400ae848a7cceebf1e469fe6a336e375677
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-05-27 11:43:25 -0500

    Finish direction page

--- src/pages/profile/Direcciones.jsx
+++ src/pages/profile/Direcciones.jsx
@@ -1,22 +1,41 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useAuth } from "../../context/auth-context";
-import { showAddress } from "../../services/address-services";
+import { createAddress, showAddress } from "../../services/address-services";
 import { Input } from "../../styles/views/Login";
+import { Autocomplete, useJsApiLoader } from '@react-google-maps/api'
 
 
 function Direcciones(){
-  const [addressDetail, setAddressDetail] = useState(null);
+  const REACT_APP_GOOGLE_MAPS_API_KEY = "AIzaSyBiDrPVhypnkVtJSNrevfqlwaFJjgWE_bA"
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries: ['places'],
+  })
+  const originRef = useRef()
   const {user} = useAuth();
   useEffect(() => {
     showAddress(user.id).then(setAddressDetail);
-  }, [user.id]); //works?
+  }, [user.id]);
 
+  const [addressDetail, setAddressDetail] = useState(null);
+  
+  if (!isLoaded) {
+    return <h2>Loading...</h2>
+  }
+ 
+
+  function newAddress(){
+    const getAddress = { customer_id: user.id, address: originRef.current.value };
+    createAddress(getAddress).then(() => {window.location.reload()});
+  }
   return(
     <>
   
       <div>
-        <Input placeholder={"Ingresa direccion a laborar"} />
-        <button>Agregar</button>
+        <Autocomplete>
+          <Input type='text' innerRef={originRef} placeholder="Ingresa direccion a laborar" />
+        </Autocomplete>
+        <button onClick={()=>newAddress()}>Agregar</button>
       </div>
       {addressDetail ? (
         <>
