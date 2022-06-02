23cace7d49749cc2eff9b3c0ee3d3203a8190820
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-06-01 22:43:41 -0500

    Add calendar without multiple

---
+++ src/components/Calendar.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import DateTimePicker from 'react-datetime-picker';
+import { cart } from '../pages/ServiceStep1';
+
+export let fecha
+function CalendarPick() {
+  const [value, onChange] = useState(new Date());
+  
+
+ fecha = value;
+  console.log(fecha);
+  return (
+    <div>
+      <DateTimePicker onChange={onChange} value={value} />
+    </div>
+  );
+}
+
+export default CalendarPick
