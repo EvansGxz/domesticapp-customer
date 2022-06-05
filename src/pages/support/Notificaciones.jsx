8913dca0670222e59107c78c40364578570934b6
Esteban Gonzalez <ldarcknes@hotmail.com>
2022-06-05 13:28:20 -0500

    add notifications, home, ...

--- src/pages/support/Notificaciones.jsx
+++ src/pages/support/Notificaciones.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import HeaderViews from "../../components/HeaderViews";
 import Footer from "../../components/Footer";
 import { BasicContainer } from "../../styles/containers";
+import Notification from "../profile/Notifications";
 
 const Notificaciones = () => {
   return (
     <BasicContainer>
       <HeaderViews title="Notificaciones" />
+        <Notification />
       <Footer />
     </BasicContainer>
   );
